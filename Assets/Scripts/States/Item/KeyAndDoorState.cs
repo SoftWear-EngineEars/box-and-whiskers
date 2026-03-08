92d13ea3efab53c385c4608764d141b2be08e4d7
Korn-Jacob <jackor27@bergen.org>
2026-03-07 20:01:19 -0500

    Add key and door

---
+++ Assets/Scripts/States/Item/KeyAndDoorState.cs
@@ -0,0 +1,10 @@
+﻿public abstract class KeyAndDoorState : ItemState
+{
+    protected KeyAndDoor KeyAndDoor;
+    public KeyAndDoorState(KeyAndDoor keyAndDoor) : base(keyAndDoor)
+    {
+        KeyAndDoor = keyAndDoor;
+    }
+
+    public abstract void HandleCombinationEvent(CombinationEvent combinationEvent);
+}
