740043c22ee3077b3f7287389626561fbcff4694
JJ <jjl2008.com@gmail.com>
2026-03-08 23:10:47 -0400

    Renamed button actions to make new button scripts, added button scripts to win screen

--- Assets/Scripts/Title Screen/ButtonActions.cs
+++ Assets/Scripts/Title Screen/ButtonActions.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonActions : MonoBehaviour, IUsesDataCenter
+public class TitleButtonActions : MonoBehaviour, IUsesDataCenter
 {
     private IDataCenter _DataCenter;
 
