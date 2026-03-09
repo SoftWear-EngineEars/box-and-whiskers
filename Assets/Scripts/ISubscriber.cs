01cda99529e2b3b43d3edca03000964db7931d76
Korn-Jacob <jackor27@bergen.org>
2026-03-08 22:12:52 -0400

    my tests
    also stuff that'll be helpful for other people's tests

--- Assets/Scripts/ISubscriber.cs
+++ Assets/Scripts/ISubscriber.cs
@@ -1,9 +1,4 @@
 ﻿public interface ISubscriber<T> : IDependency<INotifier<T>>
 {
-    INotifier<T> Notifier { get; set; }
     public void ReceiveEvent(T message);
-    void IDependency<INotifier<T>>.SetDependency(INotifier<T> notifier)
-    {
-        Notifier = notifier;
-    }
 }
